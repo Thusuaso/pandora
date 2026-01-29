5804ad01748cb08c955c0751cef3f01ffcbb7462
Thusuaso <bilgiislem@mekmar.com>
2026-01-29 19:23:34 +0300

    l

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,37 +1,43 @@
 // nuxt.config.ts
-import Aura from '@primevue/themes/aura';
+import Aura from "@primevue/themes/aura";
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@primevue/nuxt-module', '@nuxtjs/seo'],
+  modules: ["@nuxtjs/tailwindcss", "@primevue/nuxt-module", "@nuxtjs/seo"],
   primevue: {
     options: {
       theme: {
         preset: Aura,
         options: {
-            darkModeSelector: '.fake-dark-mode', // Sadece aydınlık mod istiyorsak bunu ekleriz
-        }
+          darkModeSelector: ".fake-dark-mode", // Sadece aydınlık mod istiyorsak bunu ekleriz
+        },
       },
-      ripple: true
+      ripple: true,
     },
-    autoImport: true
+    autoImport: true,
   },
-  css: [
-    'primeicons/primeicons.css'
-  ],
-site: {
-    url: 'https://www.denizlipandoratemizlik.com.tr', // <-- YENİ DOMAIN BURAYA
-    name: 'Pandora Temizlik Denizli',
-    description: 'Denizli profesyonel koltuk yıkama, halı yıkama ve yerinde temizlik hizmetleri. Buharlı ve vakumlu makinelerle derinlemesine hijyen.',
-    defaultLocale: 'tr',
+  css: ["primeicons/primeicons.css"],
+  site: {
+    url: "https://www.denizlipandoratemizlik.com.tr", // <-- YENİ DOMAIN BURAYA
+    name: "Pandora Temizlik Denizli",
+    description:
+      "Denizli profesyonel koltuk yıkama, halı yıkama ve yerinde temizlik hizmetleri. Buharlı ve vakumlu makinelerle derinlemesine hijyen.",
+    defaultLocale: "tr",
   },
 
   app: {
     head: {
-      htmlAttrs: { lang: 'tr' }, // Sitenin Türkçe olduğunu belirtir
-      charset: 'utf-8',
-      viewport: 'width=device-width, initial-scale=1',
-    }
+      htmlAttrs: { lang: "tr" }, // Sitenin Türkçe olduğunu belirtir
+      charset: "utf-8",
+      viewport: "width=device-width, initial-scale=1",
+      meta: [
+        // Size verilen kodu buraya yapıştırın:
+        {
+          name: "google-site-verification",
+          content: "BURAYA_GOOGLE_DAN_ALDIĞINIZ_KOD_GELECEK",
+        },
+      ],
+    },
   },
-})
+});
